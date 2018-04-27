97ef25c4f1bec968947d490ef6152b681fee42b9
Björn Magnusson <bjornmagnusson1984@gmail.com>
2018-04-27 20:05:01 +0200

    Added npm test in pipeline

--- app.js
+++ app.js
@@ -4,18 +4,17 @@ const app = express()
 const PORT = process.env.PORT | 3000
 
 app.get('/', function (req, res) {
-  res.send({output: 'Hello World'})
+  res.send({output: 'Hello World!'})
 })
 
 app.get('/info', function (req, res) {
   res.send({
-    name:  module.exports.name,
-    version: module.exports.version
+    name:  module.exports.name | '',
+    version: module.exports.version | ''
   })
 })
 
 app.get('/health', function (req, res) {
-  console.log('Health')
   res.send({
     status: "UP"
   })
@@ -24,3 +23,7 @@ app.get('/health', function (req, res) {
 app.listen(PORT, function () {
   console.log('Example app (%s, %s) listening on port %s!', module.exports.name, module.exports.version, PORT)
 })
+
+if (process.argv.includes('**/*.spec.js')) {
+  module.exports = app
+}
