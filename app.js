ff238408f30ebab8f66ffb1956b8a834ac046e4a
Björn Magnusson <bjornmagnusson1984@gmail.com>
2018-05-02 22:35:08 +0200

    Fixed info content

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ app.get('/', function (req, res) {
 
 app.get('/info', function (req, res) {
   res.send({
-    name:  module.exports.name | '',
-    version: module.exports.version | '',
+    name:  module.exports.name,
+    version: module.exports.version,
     hostname: hostname
   })
 })
