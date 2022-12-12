b7d4309472baafb5b5305c1b1a22a8de4066546a
Björn Magnusson <bjornmagnusson1984@gmail.com>
2022-12-12 23:08:20 +0100

    Added NA as default for info

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ app.get('/', function (req, res) {
 
 app.get('/info', function (req, res) {
   res.send({
-    name:  module.exports.name || '',
-    version: module.exports.version || '',
+    name:  module.exports.name || 'N/A',
+    version: module.exports.version || 'N/A',
     hostname: hostname
   })
 })
